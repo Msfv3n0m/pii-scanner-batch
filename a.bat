8f015233f9c56f1718054607ec1cca275d3a0232
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2022-12-22 10:39:48 -0800

    Add files via upload

---
+++ a.bat
@@ -0,0 +1,23 @@
+@echo off
+:: cant scan xls/xlsx files
+echo "" > flolf.txt
+FOR /r C:\Users\ %%f IN (*.txt, *.csv, *.doc, *.docx) DO ( 
+    findstr /i "name credit ssn email birthday social" "%%f">NUL
+    IF not ERRORLEVEL 1 (
+        echo %%f >> flolf.txt
+    )
+)
+
+FOR /r C:\inetpub\ %%f IN (*.txt, *.csv, *.doc, *.docx) DO ( 
+    findstr /i "name credit card ssn email birthday social" "%%f">NUL
+    IF not ERRORLEVEL 1 (
+        echo %%f >> flolf.txt
+    )
+)
+
+FOR /r C:\xampp\ %%f IN (*.txt, *.csv, *.doc, *.docx) DO ( 
+    findstr /i "name credit ssn email birthday social" "%%f">NUL
+    IF not ERRORLEVEL 1 (
+        echo %%f >> flolf.txt
+    )
+)
